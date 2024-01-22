df8557a4f33145f75e7f76298549219a66e380f8
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-01-22 12:35:57 +0100

    Create add task form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddTaskFormComponent } from './add-task-form/add-task-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddTaskFormComponent
   ],
   imports: [
     BrowserModule,
