d95285c46aadce168d5e2ff87247102517e8f020
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-01-24 17:33:02 +0100

    Implement bases showing tasks details

--- src/app/directives/task.directive.ts
+++ src/app/directives/task.directive.ts
@@ -1,10 +1,62 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  Renderer2,
+} from '@angular/core';
+import { Task } from '../models/task';
 
 @Directive({
-  selector: '[appTask]'
+  selector: '[appTask]',
 })
 export class TaskDirective {
+  @Input()
+  task: Task = {
+    taskName: '',
+    taskDescription: '',
+    priority: 'Wysoki',
+    addTaskDate: new Date(),
+    deadlineDate: new Date(),
+  };
 
-  constructor() { }
+  isShowingDetails: boolean = false;
 
+  private descriptionP: any;
+  private startDateP: any;
+  private deadlineDateP: any;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    this.descriptionP = renderer.createElement('p');
+    this.startDateP = renderer.createElement('p');
+    this.deadlineDateP = renderer.createElement('p');
+  }
+
+  @HostListener('click') showTaskDetails(){
+    if (!this.isShowingDetails) {
+      this.descriptionP.innerHTML = this.task.taskDescription;
+      this.startDateP.innerHTML = this.task.addTaskDate;
+      this.deadlineDateP.innerHTML = this.task.deadlineDate;
+
+      this.renderer.appendChild(this.el.nativeElement, this.descriptionP);
+      this.renderer.appendChild(this.el.nativeElement, this.startDateP);
+      this.renderer.appendChild(this.el.nativeElement, this.deadlineDateP);
+
+      this.isShowingDetails = true;
+    }
+}
+  showTaskDetails2() {
+
+  }
+
+  @HostListener('mouseleave')
+  mouseLeave() {
+    if(this.isShowingDetails){
+      this.renderer.removeChild(this.el.nativeElement, this.descriptionP);
+      this.renderer.removeChild(this.el.nativeElement, this.startDateP);
+      this.renderer.removeChild(this.el.nativeElement, this.deadlineDateP);
+
+      this.isShowingDetails = false;
+    }
+  }
 }
