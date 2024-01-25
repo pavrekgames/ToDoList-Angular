86aec661592c85f03ce78e2b0c581bbdf390d930
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-01-25 11:02:00 +0100

    Implement showing and hiding settings buttons

--- src/app/task-element/task-element.component.ts
+++ src/app/task-element/task-element.component.ts
@@ -9,21 +9,14 @@ import { TaskDirective } from '../directives/task.directive';
 })
 export class TaskElementComponent {
   isDetailsHidden: boolean = true;
+  isSettingsHidden: boolean = true;
 
-  private descriptionP: any;
-  private startDateP: any;
-  private deadlineDateP: any;
-
-  constructor(private el: ElementRef, private renderer: Renderer2, private taskDirective: TaskDirective ) {
-    this.descriptionP = renderer.createElement('p');
-    this.startDateP = renderer.createElement('p');
-    this.deadlineDateP = renderer.createElement('p');
-
-  }
+  constructor(private taskDirective: TaskDirective) {}
 
   ngOnInit(): void {
     this.taskDirective.getIsDetailsHidden().subscribe((data) => {
       this.isDetailsHidden = data;
+      this.isSettingsHidden = data;
     });
   }
 
@@ -38,19 +31,19 @@ export class TaskElementComponent {
 
   tasksDetails() {
     if (this.isDetailsHidden) {
-     // this.descriptionP.innerHTML = this.task.taskDescription;
-      //this.startDateP.innerHTML = "Data dodania: " + this.task.addTaskDate.toLocaleString();
-     // this.deadlineDateP.innerHTML = "Deadline: " + this.task.deadlineDate.toLocaleString();
-
-      //this.renderer.appendChild(this.el.nativeElement, this.descriptionP);
-      //this.renderer.appendChild(this.el.nativeElement, this.startDateP);
-      //this.renderer.addClass(this.el.nativeElement, 'text-success');
-      //this.renderer.appendChild(this.el.nativeElement, this.deadlineDateP);
-      //this.renderer.addClass(this.el.nativeElement, 'text-danger');
-
       this.isDetailsHidden = false;
-    }else{
+    } else {
       this.isDetailsHidden = true;
     }
+  }
+
+  tasksSettings(){
+    if (this.isSettingsHidden) {
+      this.isSettingsHidden = false;
+    } else {
+      this.isSettingsHidden = true;
+    }
+
+    console.log("Tasks Settings");
   }
 }
